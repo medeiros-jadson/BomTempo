7e8523f0f94d9173815e944a45a9d3d27c145a55
Jadson Medeiros <jadson.medeiros@monitoratec.com.br>
2022-01-08 19:30:54 -0300

    📦 NEW: theme styled components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    primary: "#88B5CE",
+    secondary: "#FFFF",
+    border: "#848484"
+  },
+  fonts: {
+    robotoRegular: 'Roboto_400Regular',
+    robotoBold: 'Roboto_700Bold'
+  }
+}
